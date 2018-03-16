31362e6fc3fc58053ad5a0250c92317560a972f7
Denis Fedioukovitch <denis3901442@gmail.com>
2018-03-17 00:10:51 +0300

    V 1.2

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,7 @@ import png from './images/bodyFon.jpg';
 class App extends Component {
   render() {
     return (
-      <div>
         <h1>Hello React</h1>
-        <img src={png} alt=""/>
-      </div>
     );
   }
 }

1525bffd8d127eecdb5e0b48ef874ac87e4ff654
Denis Fedioukovitch <denis3901442@gmail.com>
2018-03-17 15:57:19 +0300

    PlayerPicker

--- src/components/PlayerPicker/PlayerPicker.jsx
+++ src/components/PlayerPicker/PlayerPicker.jsx
@@ -7,15 +7,36 @@ import rickUnchosen from '../../images/rick.png';
 import mortyUnchosen from '../../images/morty.png';
 
 class PlayerPicker extends Component {
-  picker = event => {
-    console.log('lol');
+    state = {
+      chosen : true,
+    }
+
+  pick = event => {
+    event.preventDefault();
+    this.setState({chosen : !this.state.chosen})
   };
 
   render(){
-    return(
-      <p onClick={this.picker} >lol</p>
-    )
+    if(this.state.chosen == true){
+      return (
+        <div onClick={this.pick}>
+          <img src={rick} data-rick className="player rick check"/>
+          <span className="rickSelected">Chosen</span>
+          <img src={mortyUnchosen} data-morty className="player morty"/>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <img src={rickUnchosen} alt="" onClick={this.pick} data-rick className="player rick check"/>
+          <img src={morty} alt="" data-morty className="player morty"/>
+          <span className="mortySelected">Chosen</span>
+        </div>
+      )
+    }
   }
+
+
 };
 
 export default PlayerPicker;
